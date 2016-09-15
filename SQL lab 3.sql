0716df0e80819e471b3fec53cad82b5f59f71697
Michael Sanzo <mikeysanzo1962@gmail.com>
2016-09-15 13:30:59 -0400

    Add files via upload

---
+++ SQL lab 3.sql
@@ -0,0 +1,35 @@
+﻿SELECT ordnum, totalUSD
+FROM Orders
+
+SELECT name, city
+FROM Agents
+WHERE name = 'Smith'
+
+SELECT name, pid, priceUSD
+FROM Products
+WHERE quantity > 201000
+
+SELECT name, city
+FROM Customers
+WHERE city = 'Duluth'
+
+SELECT name
+FROM Agents
+WHERE city != 'New York' AND city != 'Duluth'
+
+SELECT *
+FROM Products
+WHERE city != 'Dallas' AND city != 'Duluth' AND priceUSD >= 1
+
+SELECT *
+FROM Orders
+WHERE mon IN ('feb', 'mar')
+
+SELECT *
+FROM Orders
+WHERE totalUSD >= 600 AND mon = 'feb'
+
+SELECT *
+FROM Orders
+WHERE cid = 'c005'
+
